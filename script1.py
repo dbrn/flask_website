647d8ecdd02d3285c8e41bb5dc293e9a9e990da0
dbrn <54246837+dbrn@users.noreply.github.com>
2019-09-06 14:36:52 +0200

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,25 @@
+# python 3.5+
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/code/")
+def code():
+    return render_template("code.html")
+
+
+@app.route("/contacts/")
+def contacts():
+    return render_template("contacts.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
